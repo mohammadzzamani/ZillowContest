5a2464b3d8af02d1471a66f880d732ab14397d5e
mzamani <mzamani@cs.stonybrook.edu>
2017-10-06 13:59:44 -0400

    extract_tweets added

---
+++ extract_tweets_from_mysql.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import sys
+import MySQLdb
+import os
+
+
+data_path = ''
+user= 'mzamani'
+password = ''
+database = 'zillowChallenge'
+host = 'localhost'
+table = 'final_msgs_dedup'
+filename = 'zillow_messages.tsv'
+
+def connectMysqlDB(self):
+        conn = MySQLdb.connect(host, user, password, database)
+        c = conn.cursor()
+        return c
+
+
+def extract(self):
+        try:
+                cursor = connectMysqlDB()
+        except:
+                print("error while connecting to database:", sys.exc_info()[0])
+                raise
+        if(cursor is not None):
+            sql='select {0}, {1} from {2}'.format('parcelid', 'message', table)
+            cursor.execute(sql)
+            res = cursor.fetchall()
+
+            cols = ['parcelid', 'message']
+            data = pd.DataFrame(data= res, columns= cols)
+
+            data['train_test'] = 'train'
+
+            data = data[['parcelid', 'train_test' , 'message']]
+
+
+            data.to_csv(filename, sep='\t')
+
+
+extract()
