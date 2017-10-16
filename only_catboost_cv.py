38a55d25cbebf1b1416a41f34f08e1db335c886e
mzamani <mzamani@cs.stonybrook.edu>
2017-10-16 00:11:27 -0400

    concating 16 and 17

--- only_catboost_cv.py
+++ only_catboost_cv.py
@@ -54,7 +54,7 @@ def cross_validation(all_df, folds = 5):
         for estimator in ESTIMATORS:
             print ('cntr: ' , cntr)
             print ('fitting ...', strftime("%Y-%m-%d %H:%M:%S", gmtime()))
-            if cntr >5:
+            if cntr % 2 == 1:
                 estimator.fit(Xtrain, Ytrain, cat_features=cat_feature_inds)
             else:
                 estimator.fit(Xtrain, Ytrain)
@@ -119,7 +119,7 @@ train2017 = pd.merge(train2017, language, how = 'left', on = 'parcelid')
 # train2017.iloc[:, train2017.columns.str.startswith('tax')] = np.nan
 
 # print('Concat Train 2016 & 2017 ...')
-# train_df = pd.concat([train2016, train2017], axis = 0)
+train_df = pd.concat([train2016, train2017], axis = 0)
 # test_df = pd.merge(sample_submission[['ParcelId']], properties2016.rename(columns = {'parcelid': 'ParcelId'}), how = 'left', on = 'ParcelId')
 
 del properties2017#, test_df, properties2016, train2016, train2017
@@ -144,98 +144,99 @@ gc.collect();
 ESTIMATORS = [
             mean_est(),
             # GradientBoostingRegressor(n_estimators= 150, loss='lad', random_state=0, subsample=0.75, max_depth=6, max_features=0.75,  min_impurity_decrease=0.05, learning_rate=0.01),
-            GradientBoostingRegressor(n_estimators= 250, loss='lad', random_state=0, subsample=0.85, max_depth=6, max_features=0.8,  min_impurity_decrease=0.03, learning_rate=0.02),
             CatBoostRegressor(iterations=600, learning_rate=0.02, depth=6, l2_leaf_reg=3,loss_function='MAE',eval_metric='MAE',random_seed=5, rsm=0.85),
-            GradientBoostingRegressor(n_estimators= 250, loss='lad', random_state=1, subsample=0.85, max_depth=6, max_features=0.8,  min_impurity_decrease=0.04, learning_rate=0.02),
+            GradientBoostingRegressor(n_estimators= 300, loss='lad', random_state=0, subsample=0.85, max_depth=6, max_features=0.8,  min_impurity_decrease=0.03, learning_rate=0.02),
             CatBoostRegressor(iterations=600, learning_rate=0.025, depth=6, l2_leaf_reg=3,loss_function='MAE',eval_metric='MAE',random_seed=6, rsm=0.9),
-            GradientBoostingRegressor(n_estimators= 250, loss='lad', random_state=2, subsample=0.8, max_depth=6, max_features=0.75,  min_impurity_decrease=0.03, learning_rate=0.03),
+            GradientBoostingRegressor(n_estimators= 250, loss='lad', random_state=1, subsample=0.85, max_depth=6, max_features=0.8,  min_impurity_decrease=0.04, learning_rate=0.02),
             CatBoostRegressor(iterations=600, learning_rate=0.03, depth=6, l2_leaf_reg=3,loss_function='MAE',eval_metric='MAE',random_seed=7, rsm=0.9),
-            GradientBoostingRegressor(n_estimators= 200, loss='lad', random_state=3, subsample=0.75, max_depth=6, max_features=0.8,  min_impurity_decrease=0.04, learning_rate=0.03),
+            GradientBoostingRegressor(n_estimators= 250, loss='lad', random_state=2, subsample=0.8, max_depth=6, max_features=0.75,  min_impurity_decrease=0.03, learning_rate=0.03),
             CatBoostRegressor(iterations=600, learning_rate=0.025, depth=6, l2_leaf_reg=3,loss_function='MAE',eval_metric='MAE',random_seed=8, rsm=0.85),
-            GradientBoostingRegressor(n_estimators= 300, loss='lad', random_state=4, subsample=0.8, max_depth=6, max_features=0.75,  min_impurity_decrease=0.04, learning_rate=0.03),
-            CatBoostRegressor(iterations=550, learning_rate=0.025, depth=6, l2_leaf_reg=3,loss_function='MAE',eval_metric='MAE',random_seed=9, rsm=0.9)
+            GradientBoostingRegressor(n_estimators= 200, loss='lad', random_state=3, subsample=0.75, max_depth=6, max_features=0.8,  min_impurity_decrease=0.04, learning_rate=0.03),
+            CatBoostRegressor(iterations=550, learning_rate=0.025, depth=6, l2_leaf_reg=3,loss_function='MAE',eval_metric='MAE',random_seed=9, rsm=0.9),
+            GradientBoostingRegressor(n_estimators= 300, loss='lad', random_state=4, subsample=0.8, max_depth=6, max_features=0.75,  min_impurity_decrease=0.04, learning_rate=0.03)
+
             # CatBoostRegressor(iterations=530, learning_rate=0.03,depth=6, l2_leaf_reg=3,loss_function='MAE',eval_metric='MAE',random_seed=6),
             # CatBoostRegressor(iterations=600, learning_rate=0.02,depth=6, l2_leaf_reg=3,loss_function='MAE',eval_metric='MAE',random_seed=7),
             # CatBoostRegressor(iterations=500, learning_rate=0.02,depth=6, l2_leaf_reg=3,loss_function='MAE',eval_metric='MAE',random_seed=8),
             # CatBoostRegressor(iterations=400, learning_rate=0.03,depth=6, l2_leaf_reg=3,loss_function='MAE',eval_metric='MAE',random_seed=9),
         ]
 
-for train_df in [train2016, train2017]:
-    print('Remove missing data fields ...')
+# for train_df in [train2016, train2017]:
+print('Remove missing data fields ...')
 
-    missing_perc_thresh = 0.98
-    exclude_missing = []
-    num_rows = train_df.shape[0]
-    for c in train_df.columns:
-        num_missing = train_df[c].isnull().sum()
-        if num_missing == 0:
-            continue
-        missing_frac = num_missing / float(num_rows)
-        if missing_frac > missing_perc_thresh:
-            exclude_missing.append(c)
-    print("We exclude: %s" % len(exclude_missing))
+missing_perc_thresh = 0.98
+exclude_missing = []
+num_rows = train_df.shape[0]
+for c in train_df.columns:
+    num_missing = train_df[c].isnull().sum()
+    if num_missing == 0:
+        continue
+    missing_frac = num_missing / float(num_rows)
+    if missing_frac > missing_perc_thresh:
+        exclude_missing.append(c)
+print("We exclude: %s" % len(exclude_missing))
 
-    del num_rows, missing_perc_thresh
-    gc.collect();
+del num_rows, missing_perc_thresh
+gc.collect();
 
-    print ("Remove features with one unique value !!")
-    exclude_unique = []
-    for c in train_df.columns:
-        num_uniques = len(train_df[c].unique())
-        if train_df[c].isnull().sum() != 0:
-            num_uniques -= 1
-        if num_uniques == 1:
-            exclude_unique.append(c)
-    print("We exclude: %s" % len(exclude_unique))
+print ("Remove features with one unique value !!")
+exclude_unique = []
+for c in train_df.columns:
+    num_uniques = len(train_df[c].unique())
+    if train_df[c].isnull().sum() != 0:
+        num_uniques -= 1
+    if num_uniques == 1:
+        exclude_unique.append(c)
+print("We exclude: %s" % len(exclude_unique))
 
-    print ("Define training features !!")
-    exclude_other = ['parcelid', 'logerror','propertyzoningdesc']
-    train_features = []
-    for c in train_df.columns:
-        if c not in exclude_missing \
-           and c not in exclude_other and c not in exclude_unique:
-            train_features.append(c)
-    print("We use these for training: %s" % len(train_features))
+print ("Define training features !!")
+exclude_other = ['parcelid', 'logerror','propertyzoningdesc']
+train_features = []
+for c in train_df.columns:
+    if c not in exclude_missing \
+       and c not in exclude_other and c not in exclude_unique:
+        train_features.append(c)
+print("We use these for training: %s" % len(train_features))
 
-    print ("Define categorial features !!")
-    cat_feature_inds = []
-    cat_unique_thresh = 1000
-    for i, c in enumerate(train_features):
-        num_uniques = len(train_df[c].unique())
-        if num_uniques < cat_unique_thresh \
-           and not 'sqft' in c \
-           and not 'cnt' in c \
-           and not 'nbr' in c \
-           and not 'number' in c:
-            cat_feature_inds.append(i)
+print ("Define categorial features !!")
+cat_feature_inds = []
+cat_unique_thresh = 1000
+for i, c in enumerate(train_features):
+    num_uniques = len(train_df[c].unique())
+    if num_uniques < cat_unique_thresh \
+       and not 'sqft' in c \
+       and not 'cnt' in c \
+       and not 'nbr' in c \
+       and not 'number' in c:
+        cat_feature_inds.append(i)
 
-    print("Cat features are: %s" % [train_features[ind] for ind in cat_feature_inds])
+print("Cat features are: %s" % [train_features[ind] for ind in cat_feature_inds])
 
-    print ("Replacing NaN values by -999 !!")
-    train_df.fillna(-999, inplace=True)
-    # test_df.fillna(-999, inplace=True)
-
-
-    # train_df = outlier_detection(train_df, thresh = 1)
-    train_df = cats_to_int(train_df)
-    train_df = train_df[ train_features+['logerror']]
-    print ' ... ' , len(train_features) , ' , ', train_df.shape
+print ("Replacing NaN values by -999 !!")
+train_df.fillna(-999, inplace=True)
+# test_df.fillna(-999, inplace=True)
 
 
-    print ("Training time !!")
-    X_train = train_df[train_features]
-    y_train = train_df.logerror
-    print(X_train.shape, y_train.shape)
+# train_df = outlier_detection(train_df, thresh = 1)
+train_df = cats_to_int(train_df)
+train_df = train_df[ train_features+['logerror']]
+print ' ... ' , len(train_features) , ' , ', train_df.shape
 
-    # test_df['transactiondate'] = pd.Timestamp('2016-12-01')
-    # test_df = add_date_features(test_df)
-    # X_test = test_df[train_features]
-    # print(X_test.shape)
 
-    num_ensembles = 5
-    y_pred = 0.0
+print ("Training time !!")
+X_train = train_df[train_features]
+y_train = train_df.logerror
+print(X_train.shape, y_train.shape)
 
-    cross_validation(train_df)
+# test_df['transactiondate'] = pd.Timestamp('2016-12-01')
+# test_df = add_date_features(test_df)
+# X_test = test_df[train_features]
+# print(X_test.shape)
+
+num_ensembles = 5
+y_pred = 0.0
+
+cross_validation(train_df)
 
 
 
